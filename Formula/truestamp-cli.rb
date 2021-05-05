7b65aead881bacdaaa547f003cc9582547c9a43d
Glenn Rempe <glenn@truestamp.com>
2021-05-04 23:23:34 -0400

    remove explicit version

--- Formula/truestamp-cli.rb
+++ Formula/truestamp-cli.rb
@@ -2,7 +2,6 @@ class TruestampCli < Formula
   desc "Command-line client for the Truestamp API"
   homepage "https://github.com/truestamp/truestamp-cli"
   url "https://github.com/truestamp/truestamp-cli/releases/download/v0.0.1/truestamp-darwin-x86_64-lite.tar.gz"
-  version "0.0.1"
   sha256 "2161cb447efcbf305c96c07e4d4e11bad4f2a4832e7f3d0c5af06e51edbecd6f"
 
   bottle :unneeded
