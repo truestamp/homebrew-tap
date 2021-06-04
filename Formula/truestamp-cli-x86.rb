ae4c379a4b387bf251eace02a444887fbdddf7fa
Glenn Rempe <glenn@truestamp.com>
2021-06-04 19:21:35 -0400

    v0.0.4

--- Formula/truestamp-cli-x86.rb
+++ Formula/truestamp-cli-x86.rb
@@ -1,9 +1,9 @@
 class TruestampCliX86 < Formula
   desc "Command-line client for the Truestamp API for Intel x86 64 bit Macs"
   homepage "https://github.com/truestamp/truestamp-cli"
-  url "https://github.com/truestamp/truestamp-cli/releases/download/v0.0.3/truestamp-darwin-x86_64.tar.gz"
-  sha256 "fe58b339b560c3d02b96042bd24965e881ef6e938b983c2cbcf70eb388310614"
-  version "v0.0.3"
+  url "https://github.com/truestamp/truestamp-cli/releases/download/v0.0.4/truestamp-darwin-x86_64.tar.gz"
+  sha256 "2e8fe8a0751916be9250ee557e555a9e6f599f6d823178f3c63b8633046bea25"
+  version "v0.0.4"
 
   bottle :unneeded
 
